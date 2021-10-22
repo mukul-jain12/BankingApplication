0900ff2786a005de8115470fd4ef056f39992ffc
Mukul Jain <mukuljain422@gmail.com>
2021-10-22 08:57:08 +0530

    calculate savings balance

--- src/com/bankingapp/BankingApplicationMgmt.java
+++ src/com/bankingapp/BankingApplicationMgmt.java
@@ -5,6 +5,34 @@ package com.bankingapp;
 public class BankingApplicationMgmt {
 	public static void main(String args[]) {
 		System.out.println("Welcome To The Banking Application");
-		SavingsAccount savAcc = new SavingsAccount();
+		
+		//saving account object
+		SavingsAccount saver1 = new SavingsAccount();
+		SavingsAccount saver2 = new SavingsAccount();
+		
+		//defined different saving balance
+		saver1.setSavingsBalance(2000.00);
+		saver2.setSavingsBalance(3000.00);
+		
+		//modify annual interest rate from 0 to 4%
+		SavingsAccount.modifyInterestRate(0.04);
+		
+		//calculating monthly interest
+		saver1.calculateMonthlyInterest();
+		saver2.calculateMonthlyInterest();
+
+		//print new balance when ann. int. is 5%
+		System.out.printf("New Balance for Saver1 with interest of 0.04 is " + saver1.getSavingsBalance() + "\n");
+		System.out.printf("New Balance for Saver2 with interest of 0.04 is " + saver2.getSavingsBalance() + "\n");
+		
+		//modify annual interest rate from 4% to 5%		
+		SavingsAccount.modifyInterestRate(0.05);
+		
+		saver1.calculateMonthlyInterest();
+		saver2.calculateMonthlyInterest();
+		
+		//print new balance when ann. int. is 5%
+		System.out.printf("New Balance for Saver1 with interest of 0.05 is " + saver1.getSavingsBalance() + "\n");
+		System.out.printf("New Balance for Saver2 with interest of 0.05 is " + saver2.getSavingsBalance() + "\n");
 	}
 }
--- src/com/bankingapp/SavingsAccount.java
+++ src/com/bankingapp/SavingsAccount.java
@@ -1,13 +1,45 @@
 package com.bankingapp;
 
-class SavingsAccount {
+//SavingAccount class
+public class SavingsAccount {
 	public static double annualInterestRate;
+	private double savingsBalance;
 
+	public SavingsAccount(double annIntRate, double savingsBalanace) {
+		annualInterestRate = annIntRate;
+		savingsBalance = savingsBalanace;
+	}
+	
+	//
 	public SavingsAccount() {
-		annualInterestRate = 0.0;
+		annualInterestRate = 0;
+		savingsBalance = 0;
 	}
 
-	public static double getAnnualInterestRate() {
+	//setter method
+	public void setSavingsBalance(double newBalance) {
+		savingsBalance = newBalance;
+	}
+	
+	//getter method
+	public double getSavingsBalance() {
+		return savingsBalance;
+	}
+	
+	//getter method
+	public double getAnnualInterestRate() {
 		return annualInterestRate;
+	}
+	
+	//return the monthly interest
+	public double calculateMonthlyInterest() {
+		double annIntRate = (savingsBalance * annualInterestRate/12);
+		savingsBalance = savingsBalance + annIntRate;
+		return annIntRate;
+	}
+
+	//modified interest rate
+	public static void modifyInterestRate(double newInteresRate) {
+		annualInterestRate = newInteresRate;
 	}
 }
